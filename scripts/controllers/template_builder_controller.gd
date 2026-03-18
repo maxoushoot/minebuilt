d124ca03ca838997116adb069673ddc936a62dba
maxoushoot <endkill73@gmail.com>
2026-03-18 16:37:30 +0100

    Bootstrap clean Godot skeleton with mode routing and core services

--- scripts/controllers/template_builder_controller.gd
+++ scripts/controllers/template_builder_controller.gd
@@ -3,12 +3,16 @@ class_name TemplateBuilderController
 
 const BUILD_AREA_SIZE := Vector2i(100, 100)
 
-var _validation := TemplateValidationService.new()
-
 @onready var _status_label: Label = %StatusLabel
 
 func _ready() -> void:
 	_status_label.text = "Template zone ready (%dx%d)" % [BUILD_AREA_SIZE.x, BUILD_AREA_SIZE.y]
 
 func validate_sample(template: BuildingTemplateDefinition, archetype: TemplateArchetypeDefinition) -> Dictionary:
-	return _validation.validate_template(template, archetype)
+	return AppServices.template_validation.validate_template(template, archetype)
+
+func _on_back_to_menu_pressed() -> void:
+	AppState.set_mode(AppState.MODE_MENU)
+
+func _on_open_world_mode_pressed() -> void:
+	AppState.set_mode(AppState.MODE_WORLD)
